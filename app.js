5da95af505428d2c050052cbf68684aa0bcdf332
Ahernandezl0889 <ahernandezl0889@gmail.com>
2019-09-04 20:46:39 -0400

    testing commit

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 // default value for title local
 app.locals.title = 'Flixtino for Ironhack';
 
-
+// this is a test
 
 const index = require('./routes/index');
 app.use('/', index);
