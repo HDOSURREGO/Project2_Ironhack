8a7e95d85dacfd1a357d50098b4404fbc62bd5ed
Henry Hoyos <hdosurrego@gmail.com>
2019-09-04 21:00:20 -0400

    made changes to index route and hbs and movie-details

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const router  = express.Router();
+const Movie = require('../models/Movie')
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  res.render('index');
-});
+// router.get('/', (req, res, next) => {
+//   res.render('index');
+// });
 
 router.get('/', (req, res, next) => {
   Movie
   .find()
   .then((result)=>{
-  res.render('index', {listOfMovies: result});
+  res.render('movie-views/movies', {listOfMovies: result});
   })
-  
-  console.log(result)
- 
   .catch((err)=>{
     next(err);
   })
