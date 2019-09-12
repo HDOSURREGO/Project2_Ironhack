e954618618dd17db258a098eaf8d57ab3043b0f4
Henry Hoyos <hdosurrego@gmail.com>
2019-09-12 19:35:55 -0400

    movie details working less css

--- routes/movie-routes.js
+++ routes/movie-routes.js
@@ -13,19 +13,20 @@ router.get('/new-movie', (req, res, next) =>{
   res.render('movie-views/new-movie');
 });
 
-//This route creates a new Movie in the MongoDB
-//with the information entered in the form on the 
-//"new-movie" view
+// This route creates a new Movie in the MongoDB
+// with the information entered in the form on the 
+// "new-movie" view
 router.post('/new-movie', upload.single('poster-file'), (req, res, next) => {
   let newMovie = req.body;
-  newMovie.image = 'images/avatar.jpg';
-  if(req.file){
-    newMovie.image = req.file.url;
-  }
+  newMovie.image = 'images/avatar.png';
+  // if(req.file){
+  //   newMovie.image = req.file.url;
+  // }
+  newMovie.image = newMovie.posterfile;
     Movie
     .create(newMovie)
     .then(newMovie=> {
-      res.render('movie-views/moviefile-upload');
+      res.redirect(`/movies/details/${newMovie._id}`);
     })
     .catch(err => console.log("Error while creating a new movie: ", err));
 
@@ -34,25 +35,17 @@ router.post('/new-movie', upload.single('poster-file'), (req, res, next) => {
 
 });
 
-router.post('/moviefile-upload', localstore.single('video-file'), (req, res, next)=>{
-  const newMovieFile = new Movie({
-      movieFile: req.file.filename,
-      description: req.body.description
+
+router.get('/details/:id', (req, res, next)=>{
+  
+  Movie.findById(req.params.id)
+  .then((result)=>{
+    res.render('movie-views/movie-details', {theMovie: result})
   })
-      cloudinary.v2.uploader.upload('./public/uploads/'+req.file.filename,
-       {resource_type: 'video'}, function(error,result){
-          console.log("=============================================", result, error)
-          newMovieFile.movieFile = result.url;
-          newMovieFile.save()
-          .then((response)=>{
-              res.redirect('../index');
-          })
-          .catch((err)=>{
-              next(err);
-          });
-           
-      })
-});
+  .catch((err)=>{
+    next(err)
+  })
+  })
 
 module.exports = router;
 
